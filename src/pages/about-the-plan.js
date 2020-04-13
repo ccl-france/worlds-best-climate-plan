1049c92c6d972d32798144f108c648d578150a53
Grégoire Lodi <gregoire.lodi@gmail.com>
2020-04-13 11:49:07 +0200

    faq translation

--- src/pages/about-the-plan.js
+++ src/pages/about-the-plan.js
@@ -13,76 +13,76 @@ const P = ({ children }) => (
 
 const faq = [
   [
-    'Hvorfor skal Danmark have verdens bedste klimaplan?',
-    'Danmarks politikere er blevet enige: Danmark skal reducere sine udledninger med 70 pct. ift. niveauet i 1990. Det kan sagtens nås, selvom det er ambitiøst. Ja, faktisk meget ambitiøst. Det eneste andet land, der er så ambitiøs er Sverige. \n\n Hvis man kigger på verdens samlede drivhusgasudledninger, så udgør Danmark kun en brøkdel. I 2014 udledte Danmark 0.27% pct af verdens samlede drivhusgasudledninger. Men hvorfor skal vi så gøre noget overhovedet? \n\n Her kommer den ubekvemme sandhed: Hvis vi kigger på vores nuværende klimaaftryk per indbygger, så er Danmark et af de slemmeste lande - og det er endnu værre, hvis vi kigger på al den historiske udledning - altså den forurening, som Danmark har været skyld i siden det industrielle gennembrud. Det giver os et ansvar for at gøre det bedre. \n\n Vi er en af de slemmeste drenge i klassen, men samtidig har vi kæmpe ambitioner om at blive bedre, og det giver os en unik mulighed. \n\n Danmark, hvis vi vælger den bedste klimaplan, kan vise hele omverdenen, at en grøn omstilling kan lade sig gøre - og dermed inspirere alle andre lande til at gøre det samme.',
+    'Pourquoi la France devrait-elle avoir le meilleur plan climatique du monde ?',
+    "Les politiciens danois sont d'accord: le Danemark doit réduire ses émissions de 70%. par rapport au niveau de 1990. Il peut être facilement atteint, même s'il est ambitieux. Oui, en fait très ambitieux. Le seul autre pays aussi ambitieux est la Suède. \n \n En ce qui concerne les émissions mondiales totales de gaz à effet de serre, le Danemark n'est qu'une fraction. En 2014, le Danemark a émis 0,27% du total des émissions mondiales de gaz à effet de serre. Mais pourquoi devrions-nous faire quoi que ce soit? \n \n Voici la vérité qui dérange: si nous regardons notre empreinte climatique actuelle par habitant, le Danemark est l'un des pires pays - et pire encore si nous regardons toutes les émissions historiques - c'est-à-dire la pollution que le Danemark est à blâmer depuis la percée industrielle. Cela nous donne la responsabilité de faire mieux. \n \nNous sommes l'un des pires garçons de la classe, mais en même temps, nous avons d'énormes ambitions pour aller mieux, ce qui nous donne une opportunité unique. \n \n Le Danemark, si nous choisissons le meilleur plan climatique, peut montrer au monde entier qu'une transition verte peut être réalisée - et inspirer ainsi tous les autres pays à faire de même.",
   ],
   [
-    'Er en pris på forurening nok til at give os 70 pct. CO2-reduktion i 2030?',
-    'Nej, fordi vi har mægtig travlt. Men det kan få os langt og bedst muligt hen ad vejen. Derfor skal politikerne også investere i forskning, infrastruktur og incitamenter for at få de sidste procenter med.',
+    'Un prix de la pollution est-il suffisant pour nous permettre une réduction de 70% du CO2 en 2030 ?',
+    'Non, car nous sommes pressés. Mais cela peut déjà nous faire énormément progresser. Par conséquent, les politiciens doivent également investir dans la recherche, les infrastructures et les incitations pour permettre de gagner les derniers pourcentages.',
   ],
   [
-    'Hvad er ulemperne? Det lyder for godt til at være sandt.',
-    'Desværre er afgiften ikke nok i sig selv til at nå 70%. Derfor skal politikerne også træffe hårde beslutninger om fx. bilfrizoner, ladeinfrastruktur, bygningsreglement, olieudvinding og forskning',
+    'Quels sont les inconvénients ? Cela semble trop beau pour être vrai.',
+    'Malheureusement, la taxe ne suffit pas en soi pour atteindre 70%. Par conséquent, les politiciens doivent également prendre des décisions difficiles, par exemple: zones sans voiture, infrastructure de recharge, réglementation de la construction, ...',
   ],
   [
-    'Skal løsningen ikke bruges i hele EU eller hele verden før den er effektiv?',
-    'Det vil være den ideelle løsning, hvis verdens bedste klimaplan blev implementeret i hele verden eller i hele EU. Uanset hvad der sker udenfor vores grænser, er økonomerne stadigvæk overbeviste om, at det er bedst for Danmark at bruge planen, fordi det er den billigste og bedste vej for at nå vores mål, og så kan vi også vise vejen for de andre lande.',
+    "La solution ne devrait-elle pas être utilisée dans l'ensemble de l'UE ou du monde avant d'être efficace ?",
+    "Ce serait la solution idéale si le meilleur plan climatique du monde était mis en œuvre dans le monde ou déjà dans l'UE. Quoi qu'il arrive en dehors de nos frontières, les économistes sont toujours convaincus qu'il est préférable que la France utilise le plan car c'est le moyen le moins cher et le meilleur pour atteindre nos objectifs, et nous pouvons également montrer la voie aux autres pays.",
   ],
   [
-    'Skal man regne, hvor meget CO2 der er i ethvert produkt?',
-    'Nope. Her består tricket i at lægge afgiften så tæt på forureningskilden som muligt; på fossile brændsler, drøvtyggere og materialerne til forurenende produktion såsom cement. Når prisen på CO2 bliver indregnet i disse produkter fra starten, vil afgiften rykke med fra produkt til produkt, der benytter sig af fx olie, kul eller cement.',
+    'Avez-vous besoin de calculer la quantité de CO2 dans chaque produit ?',
+    "Non. Ici, l'astuce consiste à placer la taxe le plus près possible de la source de pollution : sur les combustibles fossiles, les ruminants et les matériaux de production de polluants tels que le ciment. Lorsque le prix du CO2 est inclus dans ces produits depuis le début, la taxe passe d'un produit à l'autre en utilisant, par exemple, du pétrole, du charbon ou du ciment.",
   ],
   [
-    'Skal man fjerne alle eksisterende CO2-afgifter?',
-    'Det behøver man faktisk ikke! Mange af vores eksisterende klimaafgifter i dag bruges til at finansiere vores velfærdsstat, og at fjerne dem vil kræve, at man finder nye indtægtskilder. Derfor er det nemmeste blot at lægge dem ovenpå de eksisterende afgifter - og lade politikerne tænke, hvordan de skal finansiere velfærden i fremtiden.',
+    'Faut-il éliminer toutes les taxes sur le CO2 existantes ?',
+    "Vous n'êtes pas obligé de le faire! Aujourd'hui, bon nombre de nos taxes climatiques actuelles sont utilisées pour financer notre État providence et leur suppression nécessitera de trouver de nouvelles sources de revenus. Par conséquent, il est plus facile de simplement les ajouter aux impôts existants - et de laisser les politiciens réfléchir à la manière de financer le bien-être à l'avenir.",
   ],
   [
-    'Skal industrier, der allerede er dækket af ETS-kvotesystemet også betale afgiften?',
-    'Det skal de, da ETS-kvoterne er for billige til, at de kan støtte 70 pct. målet. Man kan eventuelt godt refundere det der svarer til ETS-prisen til de pågældende industrier.',
+    'Les industries déjà couvertes par le système de quotas ETS devraient-elles également payer la taxe ?',
+    "Ils le font, car les quotas de l'ETS sont trop bon marché pour supporter 70%. cible. Il est possible de rembourser l'équivalent du prix ETS aux industries concernées.",
   ],
   [
-    'Skal alle pengene gives tilbage til borgerne?',
-    'Ideelt, jep! Pengene fordeles fuldstændig ligeligt mellem alle borgere, så uanset om du er millionær eller på kontanthjælp, får du det samme beløb ind på kontoen. For de fleste vil dette beløb mere end dække omkostningerne ved højere priser på sorte varer, og jo grønnere du lever, jo gladere vil din pengepung blive. I andre lande har man valgt at bruge en andel på velfærd eller forskning - det er dog ikke hvad økonomerne anbefaler.',
+    "Faut-il rendre tout l'argent aux citoyens ?",
+    "Idéalement, oui! L'argent est réparti de manière totalement égale entre tous les citoyens, donc que vous soyez millionnaire ou que vous ayez des prestations sociales, vous obtenez le même montant sur le compte. Pour la plupart, ce montant couvrira plus que le coût des prix plus élevés des produits carbonnés, et plus vous vivez écologique, plus votre portefeuille sera heureux. Dans d'autres pays, ils ont choisi de consacrer une part au bien-être ou à la recherche - ce n'est pas ce que les économistes recommandent.",
   ],
   [
-    'Smutter alle jobs til udlandet?',
-    'Det er meget usandsynligt. For det første er mange af de andre lande også ved at blive ambitiøse (og er derfor ikke interesserede i at få klimasyndere hos dem). Fordi EU har planer om en klimatold, så vil ikke-EU lande også være ambitiøse. Der findes også muligheder for at forbedre vores konkurrenceevne. Sidst men ikke mindst betyder verdens bedste klimaplan, at vi alle får travlt med den grønne omstilling. Der kommer til at opstå mange nye muligheder, fordi grønne produkter bliver mere konkurrencedygtige.',
+    "Tous les emplois partiront-ils à l'étranger ?",
+    "C'est très peu probable. Premièrement, de nombreux autres pays deviennent également ambitieux (et ne sont donc pas intéressés à amener des pollueurs chez eux). Parce que l'UE prévoit un tarif climatique, les pays tiers seront également ambitieux. Il existe également des possibilités d'améliorer notre compétitivité. Enfin et surtout, le meilleur plan climatique du monde signifie que nous nous occupons tous de la transition verte. De nombreuses nouvelles opportunités verront le jour à mesure que les produits verts deviendront plus compétitifs.",
   ],
   [
-    'Er der andre lande, der har gjort det før?',
-    'British Columbia i Canada har gjort det med succes - og det bliver nu udrullet i hele Canada. Det er på dagsorden i flere andre lande.',
+    "Y a-t-il d'autres pays qui l'ont déjà fait ?",
+    "La Colombie-Britannique au Canada y est parvenue avec succès - et elle est maintenant déployée partout au Canada. Il est à l'ordre du jour dans plusieurs autres pays.",
   ],
   [
-    'Hvad siger industrien?',
-    'Nogle store danske virksomheder støtter en pris på forurening. Det gælder eksempelvis Nordea, Danfoss, Vestas, og Ørsted. Dansk Industri og Dansk Erhverv har generelt været imod “dumme afgifter”. Så er det heldigt, at verdens bedste klimaplan er super smart! Fordi den er billigere samfundsøkonomisk og mere forudsigelig end alternativerne, er det nærmest en selvfølge, at virksomhederne overordnet kommer til at elske planen. Nogle kommer til at brokke sig (især klimasyndere), men det er mere fordi nogle af deres forretninger ikke rigtig kan være bæredygtig - og de kan godt se at det bliver svært at være klimaneutrale i 2050. Men selve Aalborg Portland og Arla burde se fidusen i det - så kan de sælge mere af henholdsvis Futurecem og plantedrik',
+    "Que dit l'industrie ?",
+    "Certaines grandes entreprises danoises supportent un prix de la pollution. Cela s'applique, par exemple, à Nordea, Danfoss, Vestas et Ørsted. Dansk Industri et Dansk Erhverv sont généralement opposés à des \"accusations stupides\". Ensuite, il est heureux que le meilleur plan climatique du monde soit super intelligent! Parce qu'il est moins cher sur le plan socio-économique et plus prévisible que les alternatives, il est presque normal que les entreprises, dans l'ensemble, adorent le plan. Certains vont se plaindre (en particulier les pollueurs), mais c'est plus parce que certaines de leurs entreprises ne peuvent pas vraiment être durables - et ils peuvent voir qu'il sera difficile d'être climatiquement neutre en 2050. Mais Aalborg Portland et Arla eux-mêmes devraient voir l'escroquerie - alors ils peuvent vendre plus de Futurecem et de boissons végétales respectivement",
   ],
   [
-    'Hvor meget skal det koste at udlede CO2?',
-    'For at vi skal af med vores udledninger, skal prisen være tilstrækkelig høj. Klimarådet nævnte i 2018 en pris på over 800 kroner per ton udledt CO2, men det var før målet om 70 pct. blev til. Det er sandsynligt, at prisen vil være mellem 1.000 og 1.500 kroner i 2030.',
+    "Combien devrait coûter l'émission de CO2 ?",
+    "Pour nous débarrasser de nos émissions, le prix doit être suffisamment élevé. En 2018, le Conseil du climat a mentionné un prix de plus de 800 DKK par tonne de CO2 émise, mais c'était avant l'objectif de 70% a vu le jour. Il est probable que le prix se situera entre 1000 $ et 1500 $ d'ici 2030.",
   ],
   [
-    'Hvordan finansierer vi velfærdsstaten hvis afgifterne fra fossile brændsler ikke findes længere?',
-    'Hvis vi antager, at vi rent faktisk gerne vil være klimaneutrale i 2050, så kommer vi ikke til at bruge fossile brændsler, og derfor skal vi uanset hvad finde nye indtægtskilder. Det har sådan set ikke rigtigt noget med verdens bedste klimaplan at gøre.',
+    "Comment financer l'État providence si les taxes sur les combustibles fossiles ne sont plus disponibles?",
+    "En supposant que nous voulons réellement être climatiquement neutres d'ici 2050, nous n'utiliserons pas de combustibles fossiles, c'est pourquoi nous devons trouver de nouvelles sources de revenus quoi qu'il arrive. Cela n'a rien à voir avec le meilleur plan climatique du monde.",
   ],
   [
-    'Betyder det, at jeg ikke kan flyve længere?',
-    'Nej. Det betyder simpelthen bare, at du betaler den reelle pris for din flyrejse. En smuttur til Paris vil derfor blive dyrere end i dag, men hvis du ønsker at prioritere det, vil der stadig være plads i budgettet til den årlige ferie for almindelige familier. Samtidig vil en pris på CO2 skubbe flyselskaberne til at få fart på udviklingen af klimavenlige brændstoffer og flyvemaskiner, som vil gøre grønne rejser mulige i fremtiden.',
+    "Est-ce à dire que je ne peux plus prendre l'avion ?",
+    "Non. Cela signifie simplement que vous payez le prix réel de votre vol. Un voyage à Copenhague sera donc plus cher qu'aujourd'hui, mais si vous voulez lui donner la priorité, il y aura toujours de la place dans le budget pour les vacances annuelles pour les familles ordinaires. Dans le même temps, un prix du CO2 poussera les compagnies aériennes à accélérer le développement de carburants et d'avions respectueux du climat, ce qui rendra possible les voyages écologiques à l'avenir.",
   ],
   [
-    'Kan man ikke bare forbyde tingene i stedet for?',
-    'Det kunne man, men så er det mange ting man skal forbyde, og når man forbyder ting går det hårdere ud over de svageste, der ofte ikke har råd til alternativer. Med verdens bedste klimaplan vil varens reelle pris blive brugt - og så bestemmer man selv om man synes en vare er det værd.',
+    'Ne pouvez-vous pas simplement interdire les choses à la place ?',
+    "Vous pourriez, mais il y a beaucoup de choses à interdire, et quand vous interdisez les choses, cela va au-delà des plus faibles, qui souvent ne peuvent pas se permettre des alternatives. Avec le meilleur plan climatique du monde, le prix réel du produit sera utilisé - et ensuite vous décidez si vous pensez qu'un produit en vaut la peine.",
   ],
   [
-    'Kunne man ikke bare ikke sætte et klimamærke på alt og lade folk selv bestemme om de vil købe dem?',
-    'Det vil være en god start, men det er meget usandsynligt, at det vil gøre en forskel. Der er en grund til, at man nu sætter afgiften på cigaretter op, selvom der har være grimme billeder på indpakningen i et par år.',
+    'Ne pourriez-vous pas simplement mettre une étiquette climatique sur tout et laisser les gens décider eux-mêmes de les acheter ?',
+    "Ce sera un bon début, mais il est peu probable qu'il fasse une différence. Il y a une raison pour laquelle la taxe est maintenant facturée sur les cigarettes, bien qu'il y ait des images horribles sur l'emballage depuis quelques années.",
   ],
   [
-    'Er det ikke bare en skjult måde at lave borgerløn på?',
-    'At give pengene tilbage gennem den grønne check er kun en metode at gøre det på. Man kunne også vælge at reducere andre afgifter, eller at hæve personfradraget. Beløbene vil dog ikke være særlige store for de fleste, så det er usandsynligt, at det vil påvirke borgernes bankkonto på en måde, der vil gøre at de ikke vil i arbejde.',
+    "N'est-ce pas juste une façon cachée de faire payer les citoyens ?",
+    "Rendre l'argent par le biais du chèque vert n'est qu'une façon de le faire. Vous pouvez également choisir de réduire d'autres impôts ou d'augmenter l'allocation personnelle. Cependant, les montants ne seront pas particulièrement élevés pour la plupart, de sorte qu'il est peu probable qu'ils affectent le compte bancaire des citoyens d'une manière qui les dissuadera de travailler.",
   ],
   [
-    'Kan vi ikke bare støtte virksomhederne gennem den grønne omstilling i stedet?',
-    'Det kunne man, men det er en dårligere løsning. Det er det fordi pengene kommer fra den fælles kasse (altså dine skattepenge), og fordi det er rigtig svært at gøre nok for at nå de 70 pct. Hvert år ville politikerne skulle beregne, hvor meget CO2 man har sparet, og derefter lave nye tiltag i stedet for blot at skrue op for afgiften. Det er uoverskueligt - og på ingen måde ambitiøst nok.',
+    'Ne pouvons-nous pas simplement soutenir les entreprises tout au long de la transition verte ?',
+    "C'est possible, mais c'est une pire solution. C'est parce que l'argent provient du fonds commun (c'est-à-dire votre impôt) et parce qu'il est vraiment difficile d'en faire assez pour atteindre les 70%. Chaque année, les politiciens devraient calculer combien de CO2 ils ont économisé, puis prendre de nouvelles mesures au lieu de simplement augmenter la taxe. C'est clair - et en aucun cas assez ambitieux.",
   ],
 ];
 
@@ -388,7 +388,7 @@ const ThePlan = () => {
       </section>
       <section className="w-full max-w-4xl mx-auto py-16 sm:pb-32 px-6">
         <div className="max-w-2xl">
-          <h2 className="text-2xl my-6 font-bold">Spørgsmål og svar</h2>
+          <h2 className="text-2xl my-6 font-bold">Foire aux questions</h2>
           <Collapse
             accordion
             className="bg-transparent border-0 text-green-900"
